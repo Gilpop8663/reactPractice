0f2349589652c4c5286ff2e36e4e4b445f909e8e
GilPOP <wolfye@naver.com>
2021-11-21 22:25:29 +0900

    coin $ and BTC conveter

--- src/App.js
+++ src/App.js
@@ -1,38 +1,55 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => {
-    setToDo(event.target.value);
-  };
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [amount, setAmount] = useState(0);
+  const [select, setSelect] = useState("");
+  const [name, setName] = useState("");
   const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((current) => [toDo, ...current]);
-    console.log(toDos.map((item, index) => <li key={index}>{item}</li>));
-    setToDo("");
+    //console.log({ coins: event.target.value });
+    console.log(event.target.value.split(","));
+    const [number, string] = event.target.value.split(",");
+    setSelect(number);
+    setName(string);
   };
+  const onChange = (event) => {
+    setAmount(event.target.value);
+  };
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => setCoins(json));
+    setLoading(false);
+  }, []);
   return (
     <div>
-      <h1>To Do List ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="오늘의 할 일을 적어주세요"
-        ></input>
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The Coins ! ({coins.length})</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <div>
+          Your Dollar Money :
+          <input type="number" onChange={onChange} placeholder="$" />
+          {select === "" && amount === 0 ? null : (
+            <h4>
+              {name} : {amount / select}
+            </h4>
+          )}
+          <div>
+            <select onChange={onSubmit}>
+              {coins.map((item) => (
+                <option
+                  value={[[item.quotes.USD.price], [item.name]]}
+                  key={item.id}
+                >
+                  {item.name} ({item.symbol}): ${item.quotes.USD.price}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
